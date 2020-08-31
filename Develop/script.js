8f1b6b5a2f1bd51e4df00eb0b128c7cb67bdb5e4
Timothy Forbes <timothy.forbes259@gmail.com>
2020-09-01 08:43:30 +0930

    change of process to rando, did not work so trying for loop if this does not work will look into rando more.

--- Develop/script.js
+++ Develop/script.js
@@ -15,20 +15,27 @@ generateBtn.addEventListener("click", writePassword);
 
 // psuedocode test and trial.
 //Password object, all valuees which can be randomly generated can be accessed from here.
-  const lowerCase = ['a','b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j','k','l','m','n', 'o', 'p','q','r','s','t','u','v','w','x','y','z',];
-  const integer =['0','1','2','3','4','5','6','7','8','9'];
-  const specialCharacters = ['~','`','!','@', '#', '$', '%','^', '&', '*', '(', ')','-','_','+','=', '{', '[',']','}','|',":",';','"',"'",'<',',','>','.','?', '/'];
-//Password Criteria This section will bring up a group of boxes to request the user what conditions they wish to use for their password.
+  var lowerCase = ['a','b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j','k','l','m','n', 'o', 'p','q','r','s','t','u','v','w','x','y','z',];
+ var integer =['0','1','2','3','4','5','6','7','8','9'];
+  var specialCharacters = ['~','`','!','@', '#', '$', '%','^', '&', '*', '(', ')','-','_','+','=', '{', '[',']','}','|',":",';','"',"'",'<',',','>','.','?', '/'];
+ 
+  //Password Criteria This section will bring up a group of boxes to request the user what conditions they wish to use for their password. The following block is catch in case user selects cancel on all confirm boxes asking the user to at least select one.
+// All credit to answered Dec 15 '19 at 16:43 Aaron Plocharczyk at https://stackoverflow.com/questions/59340866/random-password-generator-javascript-not-working 
 var length = Number(prompt("How many characters would you like your password to be?"));
-while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+while (isNaN(length) || length < 8 || length > 128) {
+  length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+}
 
-//boolean request on conditions to randomly generate passwords
+//boolean request on conditions to randomly generate passwords following two blocks taken from https://stackoverflow.com/questions/59340866/random-password-generator-javascript-not-working. 
+/*
+* /** The following block is catch in case user selects cancel on all confirm boxes asking the user to at least select one.
+ * All credit to answered Dec 15 '19 at 16:43 Aaron Plocharczyk at https://stackoverflow.com/questions/59340866/random-password-generator-javascript-not-working 
+ */
 var uppers = confirm("Would you like to use uppercase letters?");
 var lowers = confirm("Would you like to use lowercase letters?");
 var numbers = confirm("Would you like to use numbers?");
 var symbols = confirm("Would you like to use special characters?");
 
-// The following block is catch in case user selects cancel on all confirm boxes asking the user to at least select one.
 while (!uppers && !lowers && !numbers && !symbols) {
   alert("You must select at least one character type!");
   uppers = confirm("Would you like to use uppercase letters?");
@@ -36,37 +43,48 @@ while (!uppers && !lowers && !numbers && !symbols) {
   numbers = confirm("Would you like to use numbers?");
   symbols = confirm("Would you like to use special characters?");
 }
-// password generation 
-var characterNumber = 0;
-var passwordString = "";
+// End of borrowed code
+// password generation variables 
+;
+var passwordCharArray = [
+  lowerCase,
+  specialCharacters,
+  integer,
+]
 
 //Look into turning the following into an array or create a value seperately inside an object or an array return to the function and do something else.
 
+// The following is modded from code suggested by Instructor Trent during Office Hours!
 function generatePassword () {
-  while (characterNumber <= length) {
-    if (uppers === true) {
-      var alphabetValue = Math.floor(Math.random()*lowerCase.length);
-      passwordString += lowerCase[alphabetValue].toUpperCase();
-       characterNumber = characterNumber + 1;
-       console.log(passwordString);
-    
-    else if (lowers === true) {
-      var alphabetValue = Math.floor(Math.random()*lowerCase.length);
-      passwordString += lowerCase[alphabetValue].toUpperCase();
-       characterNumber = characterNumber + 1;
-       console.log(passwordString);
-    }  else if (uppers === true) {
-      var numberValue = Math.floor(Math.random()*integer.length);
-      passwordString += integer[numberValue];
-      characterNumber = characterNumber + 1;
-      console.log(passwordString);
-      }
-      else if (uppers === true) {
-        var symbolValue = Math.floor(Math.random()*specialCharacters.length);
-        passwordString += specialCharacters[symbolValue];
-        characterNumber = characterNumber + 1;
-        console.log(passwordString);
-      }
+  var passPhrase = '';
+  for (var characterNumber = 0; characterNumber <= length; characterNumber++) {
+    if ((uppers === true) && (lowers === true) && (numbers === true) && (symbols === true))  {
+      let selector = Math.floor(Math.random())* passwordCharArray.length;
+      console.log(selector);
+      let selected =selector[Math.floor(Math.random())* selector.length];
+      console.log(selected);
+      passPhrase += selected;
+      console.log(passPhrase);
+     return passPhrase;
     }
+  } 
 }
+
+
 generatePassword();
+/* The following is an idea suggested by Instructor Trent to follow through on
+generatePassword2();
+let passCharArray = [
+  lowerCase,
+  specialCharacters,
+  integer,
+]
+// Instructor example about creating a password generator
+let passwordLength = 4
+for(let i = 0; i <= password.Length; i++) {
+  let arrayNum = Math.floor(random())* 3;
+  let charNum = Math.floor(random())* 3;
+  console.log(` Array Choiuce ${arrayNum} char choice ${charNum}`);
+  let passwordChar = passCharArray [arrayNum][charNum];
+  password = password + passwordChar; *
+ */
