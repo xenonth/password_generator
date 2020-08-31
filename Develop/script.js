d76c44d0ef1130f08c6775c8a13334e1093f4359
Timothy Forbes <timothy.forbes259@gmail.com>
2020-08-31 12:14:17 +0930

    Step two, object array nesting, and started generate password statement.

--- Develop/script.js
+++ Develop/script.js
@@ -38,38 +38,31 @@ var passwordTypes = {
   specialCharacters: ['~','`','!','@', '#', '$', '%','^', '&', '*', '(', ')','-','_','+','=', '{', '[',']','}','|',":",';','"',"'",'<',',','>','.','?', '/'],
 }
 //Password Criteria This section will bring up a group of boxes to request the user what conditions they wish to use for their password.
+var length = Number(prompt("How many characters would you like your password to be?"));
+while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+
+//boolean request on conditions to randomly generate passwords
+var uppers = confirm("Would you like to use uppercase letters?");
+var lowers = confirm("Would you like to use lowercase letters?");
+var numbers = confirm("Would you like to use numbers?");
+var symbols = confirm("Would you like to use special characters?");
+
+// The following block is catch in case user selects cancel on all confirm boxes asking the user to at least select one.
+while (!uppers && !lowers && !numbers && !symbols) {
+  alert("You must select at least one character type!");
+  uppers = confirm("Would you like to use uppercase letters?");
+  lowers = confirm("Would you like to use lowercase letters?");
+  numbers = confirm("Would you like to use numbers?");
+  symbols = confirm("Would you like to use special characters?");
+}
 
 
 
+// if conditions and prompt test
 
-// Object for checking if the user wishes to use the above arrays.
+
+
 
 function generatePassword () {
-  var length = Number(prompt("How many characters would you like your password to be?"));
-  while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
-
-  var length = Number(prompt("How many characters would you like your password to be?"));
-  while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
-  
-  //boolean request on conditions
-  var uppers = confirm("Would you like to use uppercase letters?");
-  var lowers = confirm("Would you like to use lowercase letters?");
-  var numbers = confirm("Would you like to use numbers?");
-  var symbols = confirm("Would you like to use special characters?");
-
-  // The following block is catch in case user selects cancel on all confirm boxes asking the user to at least select one.
-  while (!uppers && !lowers && !numbers && !symbols) {
-    alert("You must select at least one character type!");
-    uppers = confirm("Would you like to use uppercase letters?");
-    lowers = confirm("Would you like to use lowercase letters?");
-    numbers = confirm("Would you like to use numbers?");
-    symbols = confirm("Would you like to use special characters?");
-}
-
-  
-}
-
 
 }
-//Modded from copy and pasted section:
-
