750500d9b282ee398ed34bc8451ca97944daef00
Timothy Forbes <timothy.forbes259@gmail.com>
2020-09-02 21:25:05 +0930

    minor updates to things

--- Develop/script.js
+++ Develop/script.js
@@ -35,10 +35,10 @@ function generatePassword () {
   while (isNaN(length) || length < 8 || length > 128) {
     length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
   }
-var uppers = confirm("Would you like to use uppercase letters?");
-var lowers = confirm("Would you like to use lowercase letters?");
-var numbers = confirm("Would you like to use numbers?");
-var symbols = confirm("Would you like to use special characters?");
+  var uppers = confirm("Would you like to use uppercase letters?");
+  var lowers = confirm("Would you like to use lowercase letters?");
+  var numbers = confirm("Would you like to use numbers?");
+  var symbols = confirm("Would you like to use special characters?");
 
   while (!uppers && !lowers && !numbers && !symbols) {
     alert("You must select at least one character type!");
@@ -67,13 +67,16 @@ var symbols = confirm("Would you like to use special characters?");
     console.log(approved)
     //The following takes the String approved, randomly recombines into a array and then joins the array back into a string
 
-    //Using Possible characters for loop to generate a random password
+    //Using Possible characters for loop to generate a random character and add it to the passPhrase
   for (let i = 0; i < length; i++) {
-    let randomCharString = Math.floor(Math.random() * approved.length); 
-    let charArray = approved[randomCharString];
+    let randomCharString = rando(0, (approved.length-1)); 
+    let charArray = approved[randomCharString]; 
     let singleCharacter = rando(charArray);
     passPhrase = passPhrase + singleCharacter;
-  }
+    //if (approved 1 != undefined) {
+      //run again until it contains a number
+    }
+  //}
   // check for appropriate characters
   console.log(passPhrase);
   return passPhrase;
