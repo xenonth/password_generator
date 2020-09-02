c5af03a5421c918a2ffd09c0e55bbc729448482e
Timothy Forbes <timothy.forbes259@gmail.com>
2020-09-02 12:33:02 +0930

    latest up-0dates

--- Develop/script.js
+++ Develop/script.js
@@ -20,7 +20,7 @@ generateBtn.addEventListener("click", writePassword());
 // The following is modded from code suggested by Instructor Trent during Office Hours!
 //Check for character type functions
 
-function generatePassword () {
+
     //Password Criteria This section will bring up a group of boxes to request the user what conditions they wish to use for their password. The following block is catch in case user selects cancel on all confirm boxes asking the user to at least select one.
 // All credit to answered Dec 15 '19 at 16:43 Aaron Plocharczyk at https://stackoverflow.com/questions/59340866/random-password-generator-javascript-not-working 
 
@@ -30,42 +30,43 @@ function generatePassword () {
 * /** The following block is catch in case user selects cancel on all confirm boxes asking the user to at least select one.
  * All credit to answered Dec 15 '19 at 16:43 Aaron Plocharczyk at https://stackoverflow.com/questions/59340866/random-password-generator-javascript-not-working 
  */
+function generatePassword () {
   var length = Number(prompt("How many characters would you like your password to be?"));
   while (isNaN(length) || length < 8 || length > 128) {
     length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
-}
+  }
 var uppers = confirm("Would you like to use uppercase letters?");
 var lowers = confirm("Would you like to use lowercase letters?");
 var numbers = confirm("Would you like to use numbers?");
 var symbols = confirm("Would you like to use special characters?");
 
-while (!uppers && !lowers && !numbers && !symbols) {
-  alert("You must select at least one character type!");
-  uppers = confirm("Would you like to use uppercase letters?");
-  lowers = confirm("Would you like to use lowercase letters?");
-  numbers = confirm("Would you like to use numbers?");
-  symbols = confirm("Would you like to use special characters?");
-  }
-// For each boolean value of true it will pass the following into an object.
+  while (!uppers && !lowers && !numbers && !symbols) {
+    alert("You must select at least one character type!");
+    uppers = confirm("Would you like to use uppercase letters?");
+    lowers = confirm("Would you like to use lowercase letters?");
+    numbers = confirm("Would you like to use numbers?");
+    symbols = confirm("Would you like to use special characters?");
+    }
+// For each boolean value of true it will pass the following into an approved object.
   var passPhrase = '';
-  for (i=0; i < length; i++) {
-    if (uppers === true) {
-      passPhrase += rando ("qwertyuiopasdfghjklzxcvbnm");
-      i++;
+  var approved = {}
+  if (uppers === true) {
+      approved.lowers = "qwertyuiopasdfghjklzxcvbnm";
     }
     if (lowers === true) {
-      passPhrase += rando ("QWERTYUIOPASDFGHJKLZXCVBNM");
-      i++;
+      passPhrase += approved.uppers = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
     }
     if (numbers === true) {
-      passPhrase += rando ("1234567890");
-      i++;
-    }
+      approved.numbers = "1234567890";
+
+      }
     if (symbols === true) {
-      passPhrase += rando ("!@#$%^&*(){}[]=<>/,.");
-      i++;
+      approved.symbols = "!@#$%^&*(){}[]=<>/,.";
     }
-    console.log(passPhrase);
+  for (let i = 0; i <= length; i++) {
+    passPhrase += rando (rando(approved).value);
+        console.log(passPhrase)
   }
+  console.log(passPhrase)
 }
-writePassword ();
