993f88b3be9e6e5ee097eaa49715e2506faffa78
Timothy Forbes <timothy.forbes259@gmail.com>
2020-09-02 11:30:23 +0930

    simpler method spitting out correct values

--- Develop/script.js
+++ Develop/script.js
@@ -13,19 +13,9 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword());
 
-// psuedocode test and trial.
-//Password object, all valuees which can be randomly generated can be accessed from here.
 
-//STILL NEED TO LINK TO A BUTTON IN THE HTML FILE!!
- 
-// End of borrowed code
-// password generation variables 
-var lowerCase =  'ab', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j','k','l','m','n', 'o', 'p','q','r','s','t','u','v','w','x','y','z',];
-var upperCase = ['A','B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J','K','L','M','N', 'O', 'P','Q','R','S','T','U','V','W','X','Y','Z',];
-var numerals = ['0','1','2','3','4','5','6','7','8','9'];
-var special =  ['~','`','!','@', '#', '$', '%','^', '&', '*', '(', ')','-','_','+','=', '{', '[',']','}','|',":",';','"',"'",'<',',','>','.','?', '/'];
 
-//Look into turning the following into an array or create a value seperately inside an object or an array return to the function and do something else.
+
 
 // The following is modded from code suggested by Instructor Trent during Office Hours!
 //Check for character type functions
@@ -56,172 +46,26 @@ while (!uppers && !lowers && !numbers && !symbols) {
   numbers = confirm("Would you like to use numbers?");
   symbols = confirm("Would you like to use special characters?");
   }
-
-  var password = '';
-  for (var characterNumber = 0; characterNumber <= length; characterNumber++) {
-    // Password generation for all character types
-    if ((uppers === true) && (lowers === true) && (numbers === true) && (symbols === true))  {
-      var passwordCharArray = [
-        lowerCase,
-        upperCase,
-        numerals,
-        special,
-      ]
-      let selector = Math.floor(Math.random()* passwordCharArray.length);
-      let selected = Math.floor(Math.random()* passwordCharArray[selector];
-      password = password + passwordCharArray[selector][selected];
-      console.log(password);
+// For each boolean value of true it will pass the following into an object.
+  var passPhrase = '';
+  for (i=0; i < length; i++) {
+    if (uppers === true) {
+      passPhrase += rando ("qwertyuiopasdfghjklzxcvbnm");
+      i++;
     }
-    // Password generation for three character types
-    else if ((uppers === false) && (lowers === true) && (numbers === true) && (symbols === true)) {
-      var passwordCharArray = [
-        lowerCase,
-        numerals,
-        special,
-      ]
-        let selector = Math.floor(Math.random()* passwordCharArray.length);
-        let selected = Math.floor(Math.random() * passwordCharArray[selector].length);
-        password += passwordCharArray[selector][selected];
-        console.log(password);
-    } else if ((uppers === true) && (lowers === false) && (numbers === true) && (symbols === true)) {
-      var passwordCharArray = [
-        upperCase,
-        numerals,
-        special,
-      ]
-      let selector = Math.floor(Math.random()* passwordCharArray.length);
-      let selected = Math.floor(Math.random() * passwordCharArray[selector].length);
-      password += passwordCharArray[selector][selected];
-      console.log(password);
-
-    } else if ((uppers === true) && (lowers === true) && (numbers === false) && (symbols === true)) {
-      var passwordCharArray = [
-        lowerCase,
-        upperCase,
-        special,
-      ]
-      let selector = Math.floor(Math.random()* passwordCharArray.length);
-      let selected = Math.floor(Math.random() * passwordCharArray[selector].length);
-      password += passwordCharArray[selector][selected];
-      console.log(password);
-    } else if ((uppers === true) && (lowers === true) && (numbers === true) && (symbols === false)) {
-      var passwordCharArray = [
-        lowerCase,
-        upperCase,
-        numerals,
-      ]
-      let selector = Math.floor(Math.random()* passwordCharArray.length);
-      let selected = Math.floor(Math.random() * passwordCharArray[selector].length);
-      password += passwordCharArray[selector][selected];
-      console.log(password);
+    if (lowers === true) {
+      passPhrase += rando ("QWERTYUIOPASDFGHJKLZXCVBNM");
+      i++;
     }
-    //Password Generation for two character types
-    else if ((uppers === false) && (lowers === false) && (numbers === true) && (symbols === true)) {
-      var passwordCharArray = [
-        numerals,
-        special,
-      ]
-        let selector = Math.floor(Math.random()* passwordCharArray.length);
-        let selected = Math.floor(Math.random() * passwordCharArray[selector].length);
-        password += passwordCharArray[selector][selected];
-        console.log(password);
-
-    } else if ((uppers === true) && (lowers === false) && (numbers === false) && (symbols === true)) {
-      var passwordCharArray = [
-        upperCase,
-        special,
-      ]
-      let selector = Math.floor(Math.random()* passwordCharArray.length);
-      let selected = Math.floor(Math.random() * passwordCharArray[selector].length);
-      password += passwordCharArray[selector][selected];
-      console.log(password);
-
-    } else if ((uppers === true) && (lowers === true) && (numbers === false) && (symbols === false)) {
-      var passwordCharArray = [
-        lowerCase,
-        upperCase,
-      ]
-      let selector = Math.floor(Math.random()* passwordCharArray.length);
-      let selected = Math.floor(Math.random() * passwordCharArray[selector].length);
-      password += passwordCharArray[selector][selected];
-      console.log(password);
-
-    } else if ((uppers === false) && (lowers === true) && (numbers === true) && (symbols === false)) {
-      var passwordCharArray = [
-        lowerCase,
-        numerals,
-      ]
-      let selector = Math.floor(Math.random()* passwordCharArray.length);
-      let selected = Math.floor(Math.random() * passwordCharArray[selector].length);
-      password += passwordCharArray[selector][selected];
-      console.log(password);
-    }     
-    else if ((uppers === true) && (lowers === false) && (numbers === true) && (symbols === false)) {
-      var passwordCharArray = [
-        upperCase,
-        numerals,
-      ]
-        let selector = Math.floor(Math.random()* passwordCharArray.length);
-        let selected = Math.floor(Math.random() * passwordCharArray[selector].length);
-        password += passwordCharArray[selector][selected];
-        console.log(password);
-
-    } else if ((uppers === false) && (lowers === true) && (numbers === false) && (symbols === true)) {
-      var passwordCharArray = [
-        lowerCase,
-        special,
-      ]
-      let selector = Math.floor(Math.random()* passwordCharArray.length);
-      let selected = Math.floor(Math.random() * passwordCharArray[selector].length);
-      password += passwordCharArray[selector][selected];
-      console.log(password);
-
+    if (numbers === true) {
+      passPhrase += rando ("1234567890");
+      i++;
     }
-    // Password Generation for a single Character Type
-    else if ((uppers === true) && (lowers === false) && (numbers === false) && (symbols === false)) {
-      var passwordCharArray = [upperCase]
-        let selector = Math.floor(Math.random()* passwordCharArray.length);
-        let selected = Math.floor(Math.random() * passwordCharArray[selector].length);
-        password += passwordCharArray[selector][selected];
-        console.log(password);
-
-    } else if ((uppers === false) && (lowers === true) && (numbers === false) && (symbols === false)) {
-      var passwordCharArray = [numerals]
-      let selector = Math.floor(Math.random()* passwordCharArray.length);
-      let selected = Math.floor(Math.random() * passwordCharArray[selector].length);
-      password += passwordCharArray[selector][selected];
-      console.log(password);
-
-    } else if ((uppers === false) && (lowers === false) && (numbers === true) && (symbols === false)) {
-      var passwordCharArray = [numerals];
-      let selector = Math.floor(Math.random()* passwordCharArray.length);
-      let selected = Math.floor(Math.random() * passwordCharArray[selector].length);
-      password += passwordCharArray[selector][selected];
-      console.log(password);
-    } else if ((uppers === false) && (lowers === false) && (numbers === false) && (symbols === true)) {
-      var passwordCharArray = [special];
-      let selector = Math.floor(Math.random() * passwordCharArray.length);
-      let selected = Math.floor(Math.random() * passwordCharArray[selector].length);
-      password += passwordCharArray[selector][selected];
-      console.log(password);
+    if (symbols === true) {
+      passPhrase += rando ("!@#$%^&*(){}[]=<>/,.");
+      i++;
     }
-  } 
+    console.log(passPhrase);
+  }
 }
-
-
-/* The following is an idea suggested by Instructor Trent to follow through on
-generatePassword2();
-let passCharArray = [
-  lowerCase,
-  specialCharacters,
-  integer,
-]
-// Instructor example about creating a password generator
-let passwordLength = 4
-for(let i = 0; i <= password.Length; i++) {
-  let arrayNum = Math.floor(random())* 3;
-  let charNum = Math.floor(random())* 3;
-  console.log(` Array Choiuce ${arrayNum} char choice ${charNum}`);
-  let passwordChar = passCharArray [arrayNum][charNum];
-  password = password + passwordChar; *
- */
+writePassword ();
